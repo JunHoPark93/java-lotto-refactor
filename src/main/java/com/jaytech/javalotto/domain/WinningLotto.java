2a9c066a5e6fcba87fc8c1afa3b15a58fca058a2
JunHoPark <pjh374968@gmail.com>
2019-04-12 17:45:16 +0900

    Initial setting

---
+++ src/main/java/com/jaytech/javalotto/domain/WinningLotto.java
@@ -0,0 +1,16 @@
+package com.jaytech.javalotto.domain;
+
+public class WinningLotto {
+    private final Lotto lotto;
+    private final int bonusNo;
+
+    public WinningLotto(Lotto lotto, int bonusNo) {
+        this.lotto = lotto;
+        this.bonusNo = bonusNo;
+    }
+
+    public Rank match(Lotto userLotto) {
+        // TODO 로직 구현
+        return null;
+    }
+}
